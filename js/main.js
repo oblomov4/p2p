2f1750da34f29ec3afa91675587a3f0d35ef4f20
oblomov4 <vlad.oblomov4@gmail.com>
2025-06-14 01:11:11 +0300

    our-team

--- js/main.js
+++ js/main.js
@@ -2,11 +2,18 @@ const pPortfolio = document.querySelectorAll('.portfolio-box-img-container-p');
 
 pPortfolio.forEach((elem) => {
   const img = document.createElement('img');
-  img.src = 'images/arrow-right-white.svg'; // Путь к изображению
+  img.src = 'images/arrow-right-white.svg';
   img.alt = 'Иконка';
-  img.className = 'dynamic-icon'; // Класс для стилей
+  img.className = 'dynamic-icon';
 
-  // Добавляем пробелы и картинку после текста
-  elem.innerHTML += '&nbsp;&nbsp;&nbsp;'; // Неразрывные пробелы
+  elem.innerHTML += '&nbsp;&nbsp;&nbsp;';
   elem.appendChild(img);
+});
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 4.2,
+  loop: false,
+  spaceBetween: 35,
+  freeMode: true, // Свободное перемещение
 });
