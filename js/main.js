ef4e4918a28549c5a175c621fe6a6038cb579c5b
oblomov4 <vlad.oblomov4@gmail.com>
2025-06-14 18:41:00 +0300

    all section without adaptive

--- js/main.js
+++ js/main.js
@@ -10,10 +10,39 @@ pPortfolio.forEach((elem) => {
   elem.appendChild(img);
 });
 
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.swiper1', {
   loop: true,
   slidesPerView: 4.2,
   loop: false,
   spaceBetween: 35,
   freeMode: true, // Свободное перемещение
+});
+
+// const swiper2 = new Swiper('.swiper2', {
+//   loop: true,
+//   slidesPerView: 6.5,
+//   spaceBetween: 45,
+//   freeMode: true,
+// });
+
+const swiper2 = new Swiper('.swiper2', {
+  loop: true,
+  slidesPerView: 6.5,
+  spaceBetween: 45,
+  freeMode: true,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false, // Важно: false для полного контроля
+  },
+  speed: 10000,
+  grabCursor: true,
+  on: {
+    init: function () {
+      this.wrapperEl.style.transitionTimingFunction = 'linear';
+    },
+    touchStart: function () {
+      this.autoplay.stop(); // Мгновенная остановка
+      this.params.autoplay.disableOnInteraction = true; // Навсегда отключаем авторестарт
+    },
+  },
 });
